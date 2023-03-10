2b17752e8c5629677d1ed98970d09ed660cafe7f
lethereal <180.pedro@gmail.com>
2023-03-10 18:37:50 +0000

    feat: added INSERT CLIP1 logic

--- importar_csv.sh
+++ importar_csv.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
-psql 'postgres://lethereal:tzxaGNJ52kDe@us-west-2.aws.neon.tech/filateste'
+PSQL="psql 'postgres://lethereal:tzxaGNJ52kDe@us-west-2.aws.neon.tech/filateste'"
+
+cat Fila-de-Teste.csv | while IFS=";" read FONTE ID_PASTA CODIGO_NOME DURACAO TIPO_AUDIO IMAGEM AUDIO ULTIMO_TESTE OP_VT OP_AUDIO
+do
+  if [[ $FONTE != 'Fonte' ]]
+    then
+    
+    if [[ $FONTE == 'CLIP1' ]]
+      then
+      VIDEO_ID=$($PSQL "SELECT video_id FROM videos WHERE video_id = '$ID_PASTA'")
+      if [[ -z $VIDEO_ID ]]
+      #Se o vídeo não existe
+        then
+        INSERT_RESULT=$($PSQL "INSERT INTO videos(fonte, video_id, codigo, tipo_audio, imagem, audio, ultimate_teste, op_vt, op_audio) VALUES('$FONTE', '$ID_PASTA', '$CODIGO_NOME', '$TIPO_AUDIO', '$IMAGEM', '$AUDIO', to_date('$ULTIMO_TESTE', 'dd/mm/yyyy'), OP_VT, OP_AUDIO)")
+      #Tratamento CLIP1
+    #elif
+      #then
+      #Tratamento CLIP2
+    #else
+      #Tratamento INFOWS
+      fi
+    fi
+
+  fi
+done
