bbbcd14a2679baff0fb01ea75694d406dbbde127
lethereal <180.pedro@gmail.com>
2023-03-21 20:19:16 +0000

    feat/clip2 logic implemented

--- importar_csv.sh
+++ importar_csv.sh
@@ -3,22 +3,48 @@ PSQL="psql postgres://lethereal:HFP4Qc5zSvij@ep-rough-mode-134797-pooler.us-west
 
 CLIP1() {
   VIDEO_ID=$($PSQL "SELECT video_id FROM videos WHERE video_id = '$2'")
-
   if [[ -z "$VIDEO_ID" ]]
     #Se o vídeo não existe
     then
       if [[ "$5" = 'Pendente' || "$6" = 'Pendente' || "$7" = 'Pendente' || "$8" = 'Pendente' || "$9" = 'Pendente' || "${10}" = 'Pendente' ]]
         then
-          echo "$($PSQL "INSERT INTO videos(fonte, video_id, codigo, duracao) VALUES('$1', '$2', '$3', '$4')")"
+          RESULT="$($PSQL "INSERT INTO videos(fonte, video_id, codigo, duracao) VALUES('$1', '$2', '$3', '$4')")"
         else
-          echo "$($PSQL "INSERT INTO videos(fonte, video_id, codigo, duracao, tipo_audio, imagem, audio, ultimo_teste, op_vt, op_audio, observacoes) VALUES('$1', '$2', '$3', '$4', '$5', '$6', '$7', to_date('$ULTIMO_TESTE', 'dd/mm/yyyy'), '$9', '${10}', '${11}')")"
+          RESULT="$($PSQL "INSERT INTO videos(fonte, video_id, codigo, duracao, tipo_audio, imagem, audio, ultimo_teste, op_vt, op_audio, observacoes) VALUES('$1', '$2', '$3', '$4', '$5', '$6', '$7', to_date('$ULTIMO_TESTE', 'dd/mm/yyyy'), '$9', '${10}', '${11}')")"
       fi
-      echo "$1 | $2 | $3 | $4 | $5 | $6 | $7 | $8 | $9 | ${10} | ${11}"
+      if [[ "$RESULT" == "INSERT 0 1" ]]
+        then
+          echo "$1 | $2 | $3 | $4 inserido com sucesso"
+        else
+          echo "Falha na inserção do vídeo $1 | $2 | $3 | $4"
+      fi
     #Video existe
   fi
 }
 
-cat Fila-de-Teste.csv | while IFS=";" read FONTE ID_PASTA CODIGO_NOME DURACAO TIPO_AUDIO IMAGEM AUDIO ULTIMO_TESTE OP_VT OP_AUDIO OBSERVACOES
+CLIP2() {
+  VIDEO_ID=$($PSQL "SELECT video_id FROM videos WHERE pasta = '$2'")
+  if [[ -z "$VIDEO_ID" ]]
+    #Se o vídeo não existe
+    then
+      if [[ "$5" = 'Pendente' || "$6" = 'Pendente' || "$7" = 'Pendente' || "$8" = 'Pendente' || "$9" = 'Pendente' || "${10}" = 'Pendente' ]]
+        then
+          RESULT="$($PSQL "INSERT INTO videos(fonte, pasta, nome, duracao) VALUES('$1', '$2', '$3', '$4')")"
+        else
+          RESULT="$($PSQL "INSERT INTO videos(fonte, pasta, nome, duracao, tipo_audio, imagem, audio, ultimo_teste, op_vt, op_audio, observacoes) VALUES('$1', '$2', '$3', '$4', '$5', '$6', '$7', to_date('$ULTIMO_TESTE', 'dd/mm/yyyy'), '$9', '${10}', '${11}')")"
+      fi
+
+      if [[ "$RESULT" == "INSERT 0 1" ]]
+        then
+          echo "$1 | $2 | $3 | $4 inserido com sucesso"
+        else
+          echo "Falha na inserção do vídeo $1 | $2 | $3 | $4"
+      fi
+    #Video existe
+  fi
+}
+
+cat Fila-de-Teste.csv | while IFS=";" read -r FONTE ID_PASTA CODIGO_NOME DURACAO TIPO_AUDIO IMAGEM AUDIO ULTIMO_TESTE OP_VT OP_AUDIO OBSERVACOES
 do
   if [[ "$FONTE" != 'Fonte' ]]
     then
@@ -26,8 +52,8 @@ do
         then
           CLIP1 "$FONTE" "$ID_PASTA" "$CODIGO_NOME" "$DURACAO" "$TIPO_AUDIO" "$IMAGEM" "$AUDIO" "$ULTIMO_TESTE" "$OP_VT" "$OP_AUDIO" "$OBSERVACOES"
       
-      #elif [[ "$FONTE" == "CLIP2" ]]
-        #then
+      elif [[ "$FONTE" == "CLIP2" ]]
+        then
         #Tratamento CLIP2
           CLIP2 "$FONTE" "$ID_PASTA" "$CODIGO_NOME" "$DURACAO" "$TIPO_AUDIO" "$IMAGEM" "$AUDIO" "$ULTIMO_TESTE" "$OP_VT" "$OP_AUDIO" "$OBSERVACOES"
       #else
