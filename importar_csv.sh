5e71a8d403b125afc9259302b84120e26338a8eb
lethereal <180.pedro@gmail.com>
2023-03-21 18:12:18 +0000

    fix/UFT8 bug fixed

--- importar_csv.sh
+++ importar_csv.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
-PSQL="psql postgres://lethereal:E7GPui3DBngk@ep-lively-meadow-217791-pooler.us-west-2.aws.neon.tech/filateste -t --no-align -c"
+PSQL="psql postgres://lethereal:HFP4Qc5zSvij@ep-rough-mode-134797-pooler.us-west-2.aws.neon.tech/filateste -t --no-align -c"
 
 CLIP1() {
   VIDEO_ID=$($PSQL "SELECT video_id FROM videos WHERE video_id = '$2'")
-  if [[ -z $VIDEO_ID ]]
+
+  if [[ -z "$VIDEO_ID" ]]
     #Se o vídeo não existe
     then
-      echo "$($PSQL "INSERT INTO videos(fonte, video_id, codigo, duracao, tipo_audio, imagem, audio, ultimo_teste, op_vt, op_audio) VALUES('$1', '$2', '$3', '$4', '$5', '$6', '$7', to_date('$8', q'dd/mm/yyyy'), '$9', '${10}')")"
-      echo "$1 | $2 | $3 | $4 inserido com sucesso"
+      if [[ "$5" = 'Pendente' || "$6" = 'Pendente' || "$7" = 'Pendente' || "$8" = 'Pendente' || "$9" = 'Pendente' || "${10}" = 'Pendente' ]]
+        then
+          echo "$($PSQL "INSERT INTO videos(fonte, video_id, codigo, duracao) VALUES('$1', '$2', '$3', '$4')")"
+        else
+          echo "$($PSQL "INSERT INTO videos(fonte, video_id, codigo, duracao, tipo_audio, imagem, audio, ultimo_teste, op_vt, op_audio) VALUES('$1', '$2', '$3', '$4', '$5', '$6', '$7', to_date('$ULTIMO_TESTE', 'dd/mm/yyyy'), '$9', '${10}')")"
+      fi
+      echo "$1 $2 $3 $4 $5 $6 $7 $8 $9 ${10}"
     #Video existe
   fi
 }
 
 cat Fila-de-Teste.csv | while IFS=";" read FONTE ID_PASTA CODIGO_NOME DURACAO TIPO_AUDIO IMAGEM AUDIO ULTIMO_TESTE OP_VT OP_AUDIO
 do
-  if [[ $FONTE != 'Fonte' ]]
+  if [[ "$FONTE" != 'Fonte' ]]
     then
-
-      if [[ $FONTE == 'CLIP1' ]]
+      DURACAO_CORRIGIDA=$(echo $DURACAO | sed 's/^"* *"$//')
+      if [[ "$FONTE" == "CLIP1" ]]
         then
-          CLIP1 "$FONTE" "$ID_PASTA" "$CODIGO_NOME" "$DURACAO" "$TIPO_AUDIO" "$IMAGEM" "$AUDIO" "$ULTIMO_TESTE" "$OP_VT" "$OP_AUDIO"
+          CLIP1 "$FONTE" "$ID_PASTA" "$CODIGO_NOME" "$DURACAO_CORRIGIDA" "$TIPO_AUDIO" "$IMAGEM" "$AUDIO" "$ULTIMO_TESTE" "$OP_VT" "$OP_AUDIO"
       #elif
         #then
         #Tratamento CLIP2
